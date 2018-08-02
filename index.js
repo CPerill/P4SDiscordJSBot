dafc806ea5e7942a12b0d2a7b03680f3290e5969
Connor Perill <connorjperill@gmail.com>
2018-08-03 00:22:34 +0100

    Updated basib commands

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-// require the discord.js module
+// https://discordjs.guide/#/creating-your-bot/adding-more-commands
+// https://discord.js.org/#/docs/main/stable/class/Guild
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config/config');
+const {prefix, token} = require('./config/config');
 
-// when the client is ready, run this code
-// this event will trigger whenever your bot:
-// - finishes logging in
-// - reconnects after disconnecting
 client.on('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', (message) => {
-	console.log(message.content);
+	if(message.content.startsWith(`${prefix}ping`) ) {
+		message.channel.send('Pong!');
+	} else if(message.content.startsWith(`${prefix}bing`) ) {
+		message.channel.send('Bong!');
+	} else if(message.content === `${prefix}server` ) {
+		message.channel.send(`Server name: ${message.channel.name}\nTotal members: ${message.guild.memberCount}`);
+	}
 });
 
 // login to Discord with your app's token
-client.login(config.TOKEN);
+client.login(token);
