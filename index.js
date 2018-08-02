44e3598b4b7f200c165b498f35d13b2ba6d05eae
Connor <connorjperill@gmail.com>
2018-08-02 23:15:09 +0100

    BAsic connection

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const Discord = require('discord.js')
-const client = Discord.client()
+// require the discord.js module
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config/config');
 
+// when the client is ready, run this code
+// this event will trigger whenever your bot:
+// - finishes logging in
+// - reconnects after disconnecting
 client.on('ready', () => {
-    console.log('Ready');
+	console.log('Ready!');
 });
+
+client.on('message', (message) => {
+	console.log(message.content);
+});
+
+// login to Discord with your app's token
+client.login(config.TOKEN);
